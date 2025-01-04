dd13382a35a349334d28e013c32da5110c481e9a
main <possums@keyboardmoss.com>
2025-01-04 10:02:13 -0800

    Added dynamic linking and disabled a small number of unused modules.  This should help the engine link faster.

---
+++ custom.py
@@ -0,0 +1,15 @@
+# custom.py
+
+module_dds_enabled = "no"
+module_enet_enabled = "no"
+module_gridmap_enabled = "no"
+module_mbedtls_enabled = "no"
+module_mobile_vr_enabled = "no"
+module_multiplayer_enabled = "no"
+module_navigation_enabled = "no"
+module_openxr_enabled = "no"
+module_theora_enabled = "no"
+
+module_upnp_enabled = "no"
+module_webrtc_enabled = "no"
+module_webxr_enabled = "no"
